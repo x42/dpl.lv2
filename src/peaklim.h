87b470ea477223ef427b33953fdc8f849e5ba4ec
Robin Gareus <robin@gareus.org>
2024-04-26 01:28:59 +0200

    Update (C), true peak upsampling and parameter smoothing by YT

--- src/peaklim.h
+++ src/peaklim.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010-2018 Fons Adriaensen <fons@linuxaudio.org>
+ * Copyright (C) 2021 Robin Gareus <robin@gareus.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
